0740bd775d66e21a8aa379936e4937c8b648b3df
Jeremy Brayton <jbrayton@rethinkgroup.org>
2016-01-24 14:23:35 -0500

    Added Javascript and CSS required for form functionality. This would be applied at the theme level

---
+++ dist/google-form.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  //clean up empty tags
+  $('.google-form-wrapper p, .google-form-wrapper br')
+    .filter(function () {
+      return $(this).html() === '';
+    })
+    .remove();
+
+  //add required class to required elements
+  $('.google-form-wrapper form')
+    .find('.ss-item-required input, .ss-item-required textarea')
+    .filter(function () {
+      return jQuery(this).attr('name').match(/entry\.\d\.single/);
+    })
+    .addClass('required');
+
+  //validate the form
+  $('.google-form-wrapper form').validate({
+    submitHandler: function (form) {
+      $(form).ajaxSubmit({
+        success: function (data) {
+          if (data) {
+            $(form).hide(200, function () {
+              $(this).prev('.success-msg').fadeIn('slow');
+            });
+          }
+        },
+        error: function (data) {
+          console.error(data);
+        }
+      });
+    }
+  });
+});
